2b611d95270298eb24e17d043975fdc4ffc52eb7
Thomas Flogstad <thomas@edison.no>
2016-02-15 01:07:58 +0100

    Korrekturer

--- presentation/nyheter.jsx
+++ presentation/nyheter.jsx
@@ -11,22 +11,18 @@ import preloader from "../src/utils/preloader";
 
 const images = {
 
-    isod: require("./nyheter/IsoD_1.jpg"),
-    kilrenne: require("./nyheter/Kilrenne_1.jpg"),
-    komplettdeal: require("./nyheter/komplettdeal_1.jpg"),
-    softxtra: require("./nyheter/SoftXtra_1.jpg"),
-    systemgodkjennig: require("./nyheter/Systemgodkjenning_1.jpg"),
-    shingel: require("./nyheter/Shingel_1.jpg"),
-    gronnetak: require("./nyheter/Gronnetak_1.jpg"),
-    dampsperrer: require("./nyheter/Dampsperrer_1.jpg"),
-    takbelegg: require("./nyheter/Takbelegg_1.jpg"),
-    rorosvinduet: require("./nyheter/Rorosvinduet_1.jpg"),
-    zink: require("./nyheter/Zink_1.jpg"),
-    tape: require("./nyheter/tape_1.jpg")
+    undertage: require("./nyheter/DK_undertage.jpg"),
+    shingel: require("./nyheter/DK_shingel.jpg"),
+    radon: require("./nyheter/DK_radonsikring.jpg"),
+    vegogklima: require("./nyheter/DK_veggogklima.jpg"),
+    tapeogkleb: require("./nyheter/DK_tapeogklebesystem.jpg"),
+    tagpap: require("./nyheter/DK_tagpap.jpg"),
+    gronnetak: require("./nyheter/DK_gronnetage.jpg"),
+    platon: require("./nyheter/DK_platon.jpg")
 };
 
  
-preloader([images.isod, images.kilrenne ,images.komplettdeal, images.softxtra, images.systemgodkjennig, images.gronnetak, images.dampsperrer, images.rorosvinduet, images.takbelegg, images.zink, images.tape]);
+preloader([images.undertage, images.shingel ,images.radon, images.vegogklima, images.tapeogkleb, images.tagpap, images.dampsperrer, images.gronnetak, images.platon]);
 
 
    class News extends React.Component {
@@ -60,18 +56,14 @@ preloader([images.isod, images.kilrenne ,images.komplettdeal, images.softxtra, i
                       updatedSlide={this.updatedSlide.bind(this)}                      
 		                  transitionDuration={1000}>
 
-				      <Slide bgImage={images.isod } tickerText="Nytt underlagsbelegg med selvklebende omlegg - for skråtak ned til 6 graders takfall" />
-				      <Slide bgImage={images.kilrenne } tickerText="Komplett løsning for alle typer lektede skråtak - Effektivt system som forenkler proffens hverdag" />
-				      <Slide bgImage={images.komplettdeal } tickerText="Isola tilbyr nytt proffkonsept med komplette løsninger - Gir garantert tett og fuktsikkert bygg" />
-				      <Slide bgImage={images.softxtra } tickerText="Ny generasjon Soft vindsperre - Xtra sterk, Xtra fuktsikker og Xtra monterinsvennlig" />
-				      <Slide bgImage={images.systemgodkjennig } tickerText="Isola er først ute med SINTEF Byggforsk Teknisk Godkjenning av et komplett klimasystem" />
-				      <Slide bgImage={images.zink } tickerText="Isola styrker seg på takrenner og lanserer monteringvennlig og komplett zink-sortiment" />
-				      <Slide bgImage={images.tape } tickerText="Ny klebesterk og fleksibel vindsperre tape - Til forsegling av vindsperrer mot gjennomføringer og detaljer" />
-				      <Slide bgImage={images.takbelegg } tickerText="Asfalt takbelegg og membraner, produsert i Norge for norske værforhold" />
-				      <Slide bgImage={images.shingel } tickerText="30 års garanti på Isola Takshingel med ny og forbedret asfalt kvalitet" />
-				      <Slide bgImage={images.gronnetak } tickerText="Effektive løsninger som beskytter, drenerer og fordrøyer større mengder regnvann" />
-				      <Slide bgImage={images.dampsperrer } tickerText="Dampsperre med variabel vanndampmotstand - Sikkerhetsventil ved innestengt fukt" />
-				      <Slide bgImage={images.rorosvinduet } tickerText="Nye aluminiumsbekledde vinduer fra Røros - Moderne, solide og vedlikeholdsvennlige" />
+				      <Slide bgImage={images.undertage } tickerText="Her kommer undertage nyhed" />
+				      <Slide bgImage={images.shingel } tickerText="Her kommer shingel nyhed" />
+				      <Slide bgImage={images.radon } tickerText="Her kommer radon nyhed" />
+				      <Slide bgImage={images.vegogklima } tickerText="Her kommer væg og klima nyhed" />
+				      <Slide bgImage={images.tapeogkleb } tickerText="Her kommer tape og klæb nyhed" />
+				      <Slide bgImage={images.tagpap } tickerText="Her kommer tagpap nyhed" />
+				      <Slide bgImage={images.gronnetak } tickerText="Her kommer grønne tage nyhed" />
+				      <Slide bgImage={images.platon } tickerText="Her kommer Platon nyhed" />
 
                 </Deck>
             </div>
--- presentation/tapeogkleb.jsx
+++ presentation/tapeogkleb.jsx
@@ -10,8 +10,10 @@ import preloader from "../src/utils/preloader";
 import Interactive from "./interactive";
 
 const images = {
-    tape1: require("./tapeogkleb/Tape-indoor_1.jpg"),
-    tape2: require("./tapeogkleb/Tape-outdoor_2.jpg")
+    tape1: require("./tapeogkleb/Tape_1.jpg"),
+    tape2: require("./tapeogkleb/Tape-indoor_1.jpg"),
+    tape3: require("./tapeogkleb/Tape-outdoor_2.jpg")
+    
 
 };
 
@@ -27,8 +29,9 @@ class Master extends React.Component {
 		<Link className="deckLink" href="/"> </Link>
 
 		<Deck transition={[ "fade"]} progress="tabs" base="/tapeogkleb/" transitionDuration={800}>
-		    <Slide  bgImage={images.tape1} tab="Indoor"></Slide>
-		    <Slide  bgImage={images.tape2} tab="Outdoor"></Slide>
+		    <Slide  bgImage={images.tape1} tab="Isola Tape- og Klæbesystem"></Slide>
+		    <Slide  bgImage={images.tape2} tab="Indoor"></Slide>
+		    <Slide  bgImage={images.tape3} tab="Outdoor"></Slide>
 		</Deck>
 	    </div>
 	);
